627603683bc31478b86c67ecf98211be0af64f91
edutedu <eduardtira02@gmail.com>
2022-11-14 09:28:38 +0200

    Primul push pe git

---
+++ src/main/java/com/example/crud_car_app/CrudCarAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_car_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCarAppApplication.class, args);
+    }
+
+}
